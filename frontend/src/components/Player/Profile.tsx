d004fdeb9e59882395336b4535b680b7baa0926f
Roben490 <reuvendagaga@gmail.com>
2024-11-27 13:43:05 +0200

    adding resize funcunalits to check if isMobile

--- frontend/src/components/Player/Profile.tsx
+++ frontend/src/components/Player/Profile.tsx
@@ -1,13 +1,31 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { playerContext } from '../../context/playerContext'
 
 export default function Profile() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 640)
     const { player } = useContext(playerContext) ?? {}
 
+    useEffect(() => {
+      window.addEventListener('resize', () => setIsMobile(window.innerWidth < 640));
+      return () => window.removeEventListener('resize', () => setIsMobile(window.innerWidth < 640));
+    }, [])
+
   return (
-    <div style={{borderRadius:'50'}}>
-        <img src="" alt="" />
+    <>
+    {isMobile ?
+     <div style={{borderRadius:'20px', background: 'black'}}>
         <p>{player?.username}</p>
+        <p>{player?.email}</p>
+    </div> 
+    : 
+    <div style={{borderRadius:'20px', background: 'black'}}>
+        <p>{player?.username}</p>
+        <p>{player?.email}</p>
+        <p>{player?.score}</p>
+        <p>{player?.lives}</p>
     </div>
+    }
+    
+    </>
   )
 }
